efc471dbfd69c1c52dc43e8a081fa68add6677f0
Gabriel Maayan <gabemgem@gmail.com>
2024-10-18 13:42:58 -0400

    Added some helper scripts

--- scripts.py
+++ scripts.py
@@ -1,36 +1,61 @@
 import solvers
-import solvers2
-import solvers3
+# import solvers2_full
+# import solvers3
 import inspect
 import json
+from tqdm import tqdm
 
 from main import get_data, get_functions
 
-def create_training_set():
-    data = get_data(train=True)
+def create_training_set(solver_module, previous_training_set=None, input_data=None):
+    data = {}
+    if input_data is None:
+        data = get_data(train=True)
+    else:
+        with open(input_data, 'r') as f:
+            data = json.load(f)
+    if len(data) == 0:
+        print('no input data')
+        return
 
     training_set = {}
+    if previous_training_set is not None:
+        with open(previous_training_set, 'r') as f:
+            training_set = json.load(f)
+    print(f'Original training set size: {len(training_set)}')
 
-    definitions = {
-        function: inspect.getsource(getattr(solvers, function)) \
-        for function in get_functions(solvers.__file__)
-    }
+    definitions = []
+    with open(solver_module, 'r') as f:
+        definitions = f.read().split('\n\n')
+    if len(definitions) == 0:
+        print("didn't read any solvers")
+        return
 
-    for key, defn in definitions.items():
-        stripped_key = key.split('solve_')[1]
+    n = len(definitions)
 
-        if stripped_key not in data:
-            continue
+    for defn in tqdm(definitions, total=n):
+        try:
+            if "solve" not in defn:
+                continue
+            solvername = defn.replace('\n', '').split('(')[0].split('def ')[1]
+            key = solvername.replace('solve_', '')
 
-        stripped_defn = '\n'.join(defn.split('\n')[1:-1])
+            if key not in data:
+                continue
 
-        training_set[stripped_key] = {
-            "test": data[stripped_key]['test'],
-            "train": data[stripped_key]['train'],
-            "solver": stripped_defn
-        }
+            stripped_defn = '\n'.join(defn.split('\n')[1:-1])
 
+            training_set[key] = {
+                "test": data[key]['test'],
+                "train": data[key]['train'],
+                "solver": stripped_defn
+            }
+        except Exception as e:
+            print(e)
+            pass
+
+    print(f'New training set size: {len(training_set)}')
     with open('training_set.json', 'w') as f:
         json.dump(training_set, f)
 
-create_training_set()
+create_training_set('solvers2_full.py', 'training_set1.json', 'data2_full.json')
